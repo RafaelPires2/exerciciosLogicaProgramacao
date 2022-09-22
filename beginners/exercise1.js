020f1e1107a85221d42237fa99f9f0cea267b57b
Rafael Pires <rafaelbrpires@gmail.com>
2022-09-22 08:43:24 -0300

    first commit

---
+++ beginners/exercise1.js
@@ -0,0 +1,15 @@
+// Write a function that returns the sum of two numbers.
+
+// Example
+
+// For param1 = 1 and param2 = 2, the output should be
+// solution(param1, param2) = 3.
+
+let param1 = 1
+let param2 = 2
+
+
+function solution() {
+return param1 + param2
+}
+console.log(solution())
